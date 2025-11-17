c6891b2bf9b7ae2b0fdca5ec61c4064597a3c3a4
Devin <devinpquinn4@gmail.com>
2025-11-17 15:52:38 -0500

    beginning structure work

---
+++ Assets/Scripts/BirdGameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class BirdGameManager : MonoBehaviour
+{
+    public GameObject birdPrefab;
+    
+    private float minSpawnX = -7.8f;
+    private float maxSpawnX = 7.8f;
+    private float minSpawnY = -4.6f;
+    private float maxSpawnY = 3f;
+}
--- Assets/Scripts/BirdRandomizer.cs
+++ Assets/Scripts/BirdRandomizer.cs
@@ -11,17 +11,55 @@ public class BirdRandomizer : MonoBehaviour
     [SerializeField] private SpriteRenderer bellyRenderer;
     
     // Store assigned color indices
-    public int CrestColorIndex { get; private set; }
-    public int HeadColorIndex { get; private set; }
-    public int WingColorIndex { get; private set; }
-    public int BellyColorIndex { get; private set; }
+    [HideInInspector] public int CrestColorIndex { get; private set; }
+    [HideInInspector] public int HeadColorIndex { get; private set; }
+    [HideInInspector] public int WingColorIndex { get; private set; }
+    [HideInInspector] public int BellyColorIndex { get; private set; }
     
-    private void Start()
+    public int[] GetColors()
     {
-        RandomizeColors();
+        return new int[] { CrestColorIndex, HeadColorIndex, WingColorIndex, BellyColorIndex };
     }
     
-    private void RandomizeColors()
+    public void SetPartColor(int bodyPartIndex, int colorIndex)
+    {
+        switch (bodyPartIndex)
+        {
+            case 0:
+                CrestColorIndex = colorIndex;
+                if (crestRenderer != null)
+                    crestRenderer.color = colorPalette[colorIndex];
+                break;
+            case 1:
+                HeadColorIndex = colorIndex;
+                if (headRenderer != null)
+                    headRenderer.color = colorPalette[colorIndex];
+                break;
+            case 2:
+                WingColorIndex = colorIndex;
+                if (wingRenderer != null)
+                    wingRenderer.color = colorPalette[colorIndex];
+                break;
+            case 3:
+                BellyColorIndex = colorIndex;
+                if (bellyRenderer != null)
+                    bellyRenderer.color = colorPalette[colorIndex];
+                break;
+            default:
+                Debug.LogError("Invalid body part index!");
+                break;
+        }
+    }
+    
+    public void SetColors(int crestIndex, int headIndex, int wingIndex, int bellyIndex)
+    {
+        SetPartColor(0, crestIndex);
+        SetPartColor(1, headIndex);
+        SetPartColor(2, wingIndex);
+        SetPartColor(3, bellyIndex);
+    }
+    
+    public void RandomizeColors()
     {
         if (colorPalette.Length != 4)
         {
