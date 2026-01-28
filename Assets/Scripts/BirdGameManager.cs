608d72cf70e334a0aeb95968540403b7efebb816
Devin <devinpquinn4@gmail.com>
2026-01-28 18:43:08 -0500

    interim

--- Assets/Scripts/BirdGameManager.cs
+++ Assets/Scripts/BirdGameManager.cs
@@ -300,7 +300,7 @@ public class BirdGameManager : MonoBehaviour
         
         yield return new WaitForSeconds(0.4f);
         
-        SoundManager.PlaySound("Swoosh");
+        SoundManager.PlaySound("Swoosh", 1f);
         
         yield return new WaitForSeconds(0.1f);
         
